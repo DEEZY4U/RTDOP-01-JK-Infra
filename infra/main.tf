ab84f80e0b27401f96e30e8312995e236e4183b9
Dhireen Kumar Rajak <iamdeerajak@gmail.com>
2025-11-02 08:49:50 +0530

    Added Security Group Module

--- infra/main.tf
+++ infra/main.tf
@@ -6,3 +6,12 @@ module "networking" {
   cidr_private_subnet = var.cidr_private_subnet
   us-east-az = var.us_east_az
 }
+
+module "security_groups" {
+  source = "./security-groups"
+  vpc_id = module.networking.dev_rtdop_01_vpc_id
+  public_subnet_cidr_block = tolist(module.networking.dev_rtdop_01_public_subnets_cidr_block)
+  ec2_sg_name = "SG for SSH, HTTP & HTTPS"
+  ec2_sg_python_app = "SG for Python App Port"
+  ec2_sg_rds_name = "SG for RDS"
+}
