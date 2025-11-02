1da090568e085168564b511f4189b6198f09ecff
Dhireen Kumar Rajak <iamdeerajak@gmail.com>
2025-11-02 08:44:21 +0530

    Added SGs for Python App and RDS

--- infra/security-groups/main.tf
+++ infra/security-groups/main.tf
@@ -1,7 +1,20 @@
 variable "vpc_id" {}
 variable "ec2_sg_name" {}
 variable "public_subnet_cidr_block" {}
-variable "" {}
+variable "ec2_sg_rds_name" {}
+variable "ec2_sg_python_app" {}
+
+output "sg_ec2_sg_ssh_https_id" {
+  value = aws_security_group.ec2_sg_ssh_http.id
+}
+
+output "sg_ec2_sg_rds_id" {
+  value = aws_security_group.rds_sg.id
+}
+
+output "sg_ec2_python_app_id" {
+  value = aws_security_group.ec2_sg_python_app_port.id
+}
 
 resource "aws_security_group" "ec2_sg_ssh_http" {
   name = var.ec2_sg_name
@@ -42,5 +55,40 @@ resource "aws_security_group" "ec2_sg_ssh_http" {
 
   tags = {
     Name = "Security Group to allow SS, HTTP, & HTTPS"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name = var.ec2_sg_rds_name
+  description = "Allow access to RDS"
+  vpc_id = var.vpc_id
+
+  ingress {
+    cidr_blocks = var.public_subnet_cidr_block
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+  }
+
+  tags = {
+    Name = "Security Group to allow traffic on 3306"
+  }
+}
+
+resource "aws_security_group" "ec2_sg_python_app_port" {
+  name = var.ec2_sg_python_app
+  description = "Enable port for 5000"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "Allow traffic on port 5000"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 5000
+    to_port = 5000
+    protocol = "tcp"
+  }
+
+  tags = {
+    Name = "Security Group to allow traffic on port 5000"
   }
 }
